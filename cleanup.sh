b87c5546ae0973ef1e790f35a8bf536231073dc8
Nigel Douglas <126002808+nigeldouglas-itcarlow@users.noreply.github.com>
2024-02-04 16:18:12 +0000

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+# Privileged Pod (K01.01) && No Resource Constraints (K01.04)
+kubectl delete -f https://raw.githubusercontent.com/nigel-falco/falco-talon-testing/main/dodgy-pod.yaml
+# Read-only FS Pod (K01.02)
+kubectl delete -f https://raw.githubusercontent.com/nigeldouglas-itcarlow/Falco-MicroK8s/main/read-only-fs.yaml
+# Run as Root User (K01.03)
+kubectl delete -f https://raw.githubusercontent.com/nigel-falco/falco-talon-testing/main/RunAsRoot.yaml
